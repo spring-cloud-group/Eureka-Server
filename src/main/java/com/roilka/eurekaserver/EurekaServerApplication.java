de46d65150e85a430194a8bee055e71545553813
章辉(zhanghui1) <33326898+Roilka@users.noreply.github.com>
2020-04-07 19:29:34 +0800

    修复eureka启动

--- src/main/java/com/roilka/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/roilka/eurekaserver/EurekaServerApplication.java
@@ -2,6 +2,8 @@ package com.roilka.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
